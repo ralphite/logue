9889371637db2662c66aac57d79b75d073f52c35
Xingyi Wen <ralph.wen@gmail.com>
2026-08-02 18:31:23 -0700

    refactor(web): centralize content width tokens

---
+++ apps/web/src/components/layout.ts
@@ -0,0 +1,5 @@
+export const pageColumnClass = "mx-auto w-full max-w-[1080px] px-8 max-[640px]:px-4";
+
+export const editorColumnClass = "mx-auto w-full max-w-[960px] px-8 max-[640px]:px-5";
+
+export const readingColumnClass = "mx-auto w-full max-w-[900px] px-8 max-[640px]:px-5";
